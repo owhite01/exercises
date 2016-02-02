cd58cf440e1007b1c7b88ffd8444bc0ff8e3b1a0
owhite01 <owhite.white@googlemail.com>
2016-02-02 14:22:38 +0000

    New notes on Question 3 defining what an Abstract class is.

---
+++ week05_DP2/src/Question3.java
@@ -0,0 +1,8 @@
+
+public class Question3 {
+    //The Abstract factory design pattern is based around the idea of a main factory that creates other factory.
+    //A factory of factories. When adopting an abstract factory, a particular framework is initiated,
+    //which creates objects adhering to a particular style and at runtime this factory is paired with any
+    //concrete factory to produce
+    //objects that follow the main overarching pattern.
+}
