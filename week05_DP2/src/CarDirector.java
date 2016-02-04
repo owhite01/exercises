8e1feca88007dd947b3aabb1a8ac6246656e73b9
owhite01 <owhite.white@googlemail.com>
2016-02-04 07:31:38 +0000

    Started on the CarBuilder pattern

--- week05_DP2/src/CarDirector.java
+++ week05_DP2/src/CarDirector.java
@@ -2,6 +2,10 @@
  * Created by OliverW on 04/02/2016.
  */
 public class CarDirector {
+    CarBuilder carBuilder;
+    public CarDirector(CarBuilder carBuilder){
+        this.carBuilder = carBuilder;
+    }
 
     public void build(){
 
