62c5c960c3052938f0863a284cfe858eeee69027
owhite01 <owhite.white@googlemail.com>
2016-02-04 06:43:44 +0000

    Added some text for Q5

---
+++ week05_DP2/src/Question5.java
@@ -0,0 +1,12 @@
+/**
+ * Created by OliverW on 04/02/2016.
+ */
+public class Question5 {
+    //5. Modularity is a design technique that normally comprises dividing code up
+    //into separate individual modules. Construction bloat with reference to programming
+    //occurs when code designed to solve a problem is written in a long/convoluted way
+    //normally resulting in the wasting of resources. In this example, one of the common
+    //causes of bloat in software is overuse of OOP structures such as inheritance or many
+    //constructors causing confusing code. This problem can normally be solved by using
+    //modularity and dividing code up to make it easier to read and interpret.
+}
