3996273acad35b5dda6bd90ea1e5b453f94da4b7
owhite01 <owhite.white@googlemail.com>
2016-02-05 15:57:31 +0000

    Question 11 initial commit

---
+++ week05_DP2/src/Question11/Question11.java
@@ -0,0 +1,17 @@
+package Question11;
+
+public class Question11 {
+    //(a) The composite design pattern is normally used when you require a set of objects
+    //that behave in a similar way to a single object.
+    //The intent of this pattern is to compose objects into tree structures to
+    // represent part-whole hierarchies.
+
+    //(b) Composite lets clients treat objects and compositions of objects uniformly
+    //to ensure they are all treated the same way as an instance of an object.
+
+    //(c) Component
+    //Leaf
+    //Composite
+    //Client
+
+}
