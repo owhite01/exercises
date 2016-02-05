2041b893bef23508cf3d91b61bc222c937d4013e
owhite01 <owhite.white@googlemail.com>
2016-02-05 09:52:38 +0000

    Refactored the code into packages

---
+++ week05_DP2/src/Facade/ScheduleServer.java
@@ -0,0 +1,30 @@
+package Facade;
+
+
+public interface ScheduleServer {
+
+    void startBooting();
+
+    void readSystemConfigFile();
+
+    void init();
+
+    void initializeContext();
+
+    void initializeListeners();
+
+    void createSystemObjects();
+
+    void releaseProcesses();
+
+    void destory();
+
+    void destroySystemObjects();
+
+    void destoryListeners();
+
+    void destoryContext();
+
+    void shutdown();
+
+}
