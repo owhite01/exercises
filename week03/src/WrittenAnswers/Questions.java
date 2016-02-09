f05e77c21455a40d3f0f76203980c270adb1cc7b
owhite01 <owhite.white@googlemail.com>
2016-02-09 07:01:42 +0000

    Added some written answers for the first set of exercises on DP

---
+++ week03/src/WrittenAnswers/Questions.java
@@ -0,0 +1,35 @@
+package WrittenAnswers;
+
+public class Questions {
+    //1. In terms of usage, an abstract class shares same characteristics with interfaces
+    //but there are a number of differences, such as:
+
+    //In an abstract class, one can create constructors, whereas in an interface this is not possible.
+
+    //Interface can contain methods whose visibility must always be public, whereas in an abstract
+    //class one may use any of the access modifiers, protected, public or private (or none)
+
+    //CLasses are able to implement more than one interface, whereas the class can only extend
+    //no more than 1 abstract class in this case.
+
+
+    //2. (a) This statement is false. Interfaces are able to have no methods and if they do are
+    //referred to as markers.
+    //public interface Car {
+    //void isLocked();
+    //}
+
+    //(b) This statement is also false, Interfaces can create constant values.
+    //final int AGE = 8;
+    // It is not possible for an interface to declare instance fields in its composition.
+
+
+    //(c) Also false, constructors and interfaces do not work together.
+
+
+
+    //3. The interface specification in question 4 has this structure.
+
+
+
+}
