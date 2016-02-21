f8e987e4e3dde4650ef149bff176a7c9843fa92b
owhite01 <owhite.white@googlemail.com>
2016-02-21 11:42:33 +0000

    More scala classes for the exercises

---
+++ week04/src/Questions/Motorboat.scala
@@ -0,0 +1,15 @@
+package Questions
+
+class Motorboat {
+
+
+  def on(): String ={
+    "Motor on"
+  }
+
+  def off(): String ={
+    "Motor off"
+  }
+
+
+}
---
+++ week04/src/Questions/Sailboat.scala
@@ -0,0 +1,16 @@
+package Questions
+
+class Sailboat {
+  def main(args: Array[String]) {
+
+  }
+
+  def raise(): String = {
+    "Sails raised"
+  }
+
+  def lower(): String = {
+    "Sails lowered"
+  }
+
+}
