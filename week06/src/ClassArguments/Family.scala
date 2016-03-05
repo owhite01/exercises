cefa354fe30b5b24918797442b651cb709df6f8d
owhite01 <owhite.white@googlemail.com>
2016-03-05 10:14:35 +0000

    More scala classes on pattern matching

--- week06/src/ClassArguments/Family.scala
+++ week06/src/ClassArguments/Family.scala
@@ -1,19 +1,23 @@
 package ClassArguments
 import Questions.atomicscala.AtomicTest._
 
-class Family (name: String*) {
+class flexibleFamily (mum: String, dad: String, name: String*) {
 
   def familySize(): Int = {
-    name.size
+    if(mum.isEmpty){
+      name.size
+      if(dad.isEmpty){
+        name.size
+      }
+    }
+    name.size + 2
   }
-
-
 }
 
 object TestArgs extends App {
-  val family1 = new Family("Mum", "Dad", "Sally", "Dick")
+  val family1 = new flexibleFamily("Mum", "Dad", "Sally", "Dick")
   family1.familySize() is 4
 
-  val family2 = new Family("Dad", "Mom", "Harry")
+  val family2 = new flexibleFamily("Dad", "Mom", "Harry")
   family2.familySize() is 3
 }
