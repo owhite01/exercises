31d6fc18f7b4a71a22b4ff27710554c320e9e123
owhite01 <owhite.white@googlemail.com>
2016-03-07 18:47:14 +0000

    More scala classes case classes and parameterized types

--- week06/src/CaseClassesAndParameterizedTypesAndFuncAsObj/Person.scala
+++ week06/src/CaseClassesAndParameterizedTypesAndFuncAsObj/Person.scala
@@ -1,4 +1,4 @@
-package CaseClassesAndParameterizedTypes
+package CaseClassesAndParameterizedTypesAndFuncAsObj
 import Questions.atomicscala.AtomicTest._
 
 
@@ -40,6 +40,11 @@ object personTest extends App{
   def explicitList(vector1: Vector[Double]): List[Double] = {
     vector1.toList
   }
+
+  def explicitVector(v1: Vector[Int]): Vector[Int] = {
+    v1
+  }
+
   inferred('a', 'b', 'c') is "Vector(a, b, c)"
   //explicit('a', 'b', 'c') is "Vector(a, b, c)"
 
@@ -51,4 +56,15 @@ object personTest extends App{
 
   explicitSet(Vector(10.0, 20.0, 10.0)) is Set(10.0, 20.0)
   explicitSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
+
+  var emptyString = ""
+  val vectorOfNumbers = explicitVector(Vector(1, 2, 3, 4))
+  vectorOfNumbers.foreach(n => emptyString = emptyString + n + ",")
+  emptyString is "1,2,3,4,"
+
+  val dogAge = (y:Int) => y*7
+  dogAge(10) is 70
+
+
+
 }
